b716bfd5161590e7891ac80875b91c6d6395472f
Eliez <eliez_l@hotmail.com>
2017-12-01 11:58:20 +0100

    Add first calling test
    First call to Steam api. Gets my profile datas from Steam.

--- server.js
+++ server.js
@@ -13,3 +13,14 @@ console.log('Listening on port ' + port);
 //Request package
 var request = require('request');
 
+// First test with API
+var secret = require('./api');
+const API_KEY = secret.apiKey;
+app.get('/steam/profile', function(httpRequest, httpResponse) {
+	var url = 'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/'+'v0002/?key='+API_KEY+'&steamids=76561198006730825';
+	console.log(url);
+	request.get(url,function(error, steamHttpResponse, steamHttpBody) {
+		httpResponse.setHeader('Content-Type', 'application/json');
+		httpResponse.send(steamHttpBody);
+	});
+});	
