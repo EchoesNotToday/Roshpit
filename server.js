85ba72d13636a95eea302b5a8787bc206bf77f39
Eliez <eliez_l@hotmail.com>
2017-12-01 10:43:51 +0100

    Add Express package

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ var port = 4000;
 var server = app.listen(port);
 console.log('Listening on port ' + port);
 
-// Express
+// Express package
 var express = require('express');
 var app = express();
+
+//Request package
+var request = require('request');
