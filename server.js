d9b8f70930970c574c26bc3744c33d478420f5c2
Eliez <eliez_l@hotmail.com>
2017-12-01 11:20:49 +0100

    Fixed server
    Changed order to fix mistake made. Server didn't load because express was defined after the server config.

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
+
+// Express package
+var express = require('express');
+var app = express();
+
 //	Server initialization
 
 var port = 4000;
 
 var server = app.listen(port);
 console.log('Listening on port ' + port);
-
-// Express package
-var express = require('express');
-var app = express();
 
 //Request package
 var request = require('request');
