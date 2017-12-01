47445a827fbbfb3a790d9ffb66c5f16320dbc1e6
Eliez <eliez_l@hotmail.com>
2017-12-01 10:39:41 +0100

    Add Express to server.js

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 //	Server initialization
 
 var port = 4000;
+
 var server = app.listen(port);
 console.log('Listening on port ' + port);
+
+// Express
+var express = require('express');
+var app = express();
